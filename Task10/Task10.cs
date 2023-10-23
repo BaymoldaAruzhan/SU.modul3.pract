8b0e3a22def8942c55e702eb993dd698df31fc4b
Aru2003 <baymolda03@inbox.ru>
2023-10-23 10:24:15 +0600

    задачи

---
+++ Task10/Task10.cs
@@ -0,0 +1,44 @@
+﻿using System;
+class Task10
+{
+    static void Main()
+    {
+        Console.Write("Введите строку с малыми русскими буквами: ");
+        string input = Console.ReadLine();
+
+        string uppercaseInput = ConvertToUppercase(input);
+
+        Console.WriteLine("Преобразованная строка: " + uppercaseInput);
+    }
+
+    static string ConvertToUppercase(string input)
+    {
+        // Создаем пустую строку, в которую будем добавлять большие буквы
+        string result = "";
+
+        // Проходим по каждому символу в исходной строке
+        foreach (char character in input)
+        {
+            // Проверяем, является ли символ малой русской буквой
+            if (IsLowerCyrillic(character))
+            {
+                // Преобразуем малую букву в большую и добавляем к результату
+                char uppercaseChar = char.ToUpper(character);
+                result += uppercaseChar;
+            }
+            else
+            {
+                // Если символ не является малой русской буквой, добавляем его без изменений
+                result += character;
+            }
+        }
+
+        return result;
+    }
+
+    // Функция для проверки, является ли символ малой русской буквой
+    static bool IsLowerCyrillic(char character)
+    {
+        return character >= 'а' && character <= 'я';
+    }
+}
